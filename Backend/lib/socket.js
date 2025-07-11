6b5a3f53c1d90b7e59c72aa83de4d8a87262c429
JAGATHISH KUMAR <jagathishkumar6@gmail.com>
2025-07-11 22:47:06 +0530

    y

--- Backend/lib/socket.js
+++ Backend/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://realtime-chat-video-calling-app.vercel.app/Login&Signup"],
+    origin: ["http://localhost:5173","https://realtime-chat-video-calling-app.vercel.app"],
     credentials: true,
   },
 });
