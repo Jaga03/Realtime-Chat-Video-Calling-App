9b2c49e8cf1c3bb4d34b3ea5ac1f73de1992217d
JAGATHISH KUMAR <jagathishkumar6@gmail.com>
2025-07-11 22:03:59 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ import path from 'path'
 dotenv.config()
 
 const port = process.env.PORT || 5000;
-const __dirname = path.resolve()
+
 
 
 app.use(express.json({ limit: "10mb" }));
@@ -27,13 +27,6 @@ app.use(cors({ origin: ['http://localhost:5173'], credentials: true }))
 app.use('/api/auth',authRoutes)
 app.use('/api/message',messageRoutes)
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-  });
-}
 
 
 
