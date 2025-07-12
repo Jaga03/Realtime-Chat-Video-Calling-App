acb2f69c3201e02db7c5087da942627a860ef18c
JAGATHISH KUMAR <jagathishkumar6@gmail.com>
2025-07-12 14:40:47 +0530

    yes

--- Backend/lib/socket.js
+++ Backend/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","https://chatzup-app.vercel.app"],
+    origin: process.env.CORS_ORIGINS?.split(",") || ["http://localhost:5173", "https://chatzup-app.vercel.app"],
     credentials: true,
   },
 });
